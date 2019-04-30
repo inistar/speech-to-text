eeb80732e13a429efd2283fb8263e35219143e8b
soumyasudipta <soumya.sarangi.10@gmail.com>
2019-05-01 00:47:21 +0530

    Add files via upload

--- text.py
+++ text.py
@@ -8,11 +8,13 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'E:\\GCP\\hackstaticboss\\Service
 
 # GCS Bucket Properties
 bucket_name = 'speech-to-text-hackathon'
-source_blob_name = 'result/16/part-m-00000'
+source_blob_name = 'result/2019/part-m-00000'
 destination_file_name = 'E:\\text.csv'
+blob_name = 'result/2019'
 
 # Local Downloaded CSV File Properties
 csv_file = 'E:\\text.csv'
+
 
 def download_file(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
@@ -27,11 +29,26 @@ def download_file(bucket_name, source_blob_name, destination_file_name):
         destination_file_name))
 
 
+# This function deletes the folder where the text data is stored.
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix='result/2019')
+    for blob in blobs:
+        blob.delete()
+
+    print('Blob {} deleted.'.format(blob_name))
+
+
+# CSV to Text Function
 def csv_to_text(csv_file):
     df = pd.read_csv(csv_file)
     text = df.columns[1]
     print(text)
 
 
+# Function Call
 download_file(bucket_name, source_blob_name, destination_file_name)
+delete_blob(bucket_name, blob_name)
 csv_to_text(csv_file)
